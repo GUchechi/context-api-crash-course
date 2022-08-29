b4018576d02a3f09f4d3b422b6955b33cc5d22fe
GUchechi <godswilluchendu7@gmail.com>
2022-08-29 03:49:57 +0100

    added useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useReducer } from 'react';
 import './App.css';
+
+const reducer = (state, action) =>  {
+  switch (action.type) {
+    case "increment":
+      return{ count : state.count + 1, showText : state.showText}
+    case "toggleShowText":
+      return { count : state.count, showText: !state.showText}
+      default: 
+        return state;
+    }
+}
 
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {count : 0, showText : true});
+
   return (
     <div className="App">
-      
+      <h1>{state.count}</h1>
+
+      <button
+        onClick={() => {
+          dispatch({ type: 'increment'})
+          dispatch({ type: 'toggleShowText'})
+        }}
+      >Click here</button>
+
+      { state.showText && <p>This is a text</p>}
     </div>
   );
 }
