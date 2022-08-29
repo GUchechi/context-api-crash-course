d96050b5c6ea433ba684bddb90d5bfd53f6fa7d6
GUchechi <godswilluchendu7@gmail.com>
2022-08-29 19:09:06 +0100

    added useEffect

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ import axios from 'axios';
 function App() {
 
   const [data, setData] = useState("")
+  const [count, setCount] = useState(0)
+
+  const handler = () => {
+    setCount(count + 1);
+  }
 
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/photos").then((response) => {
       setData(response.data[0].title)
       console.log('API WAS CALLED')
-    })
-  }, [])
+    });
+  }, [count])
 
   return (
     <div className="App">
       <h1>Hello world : {data}</h1>
+      <h1>{count}</h1>
+
+      <button onClick={handler}>Click</button>
 
     </div>
   );
